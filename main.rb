088bd414b69e4eefb6294a4642ad85d85c8dfc40
Marcel Reuter <marcel_reuter@arcor.de>
2017-12-03 13:50:19 +0100

    add function for remove spaces

--- main.rb
+++ main.rb
@@ -20,10 +20,19 @@ get '/' do
   get_hashtags = HTTParty.get("http://ci-slave1.virtapi.org:9494/json")
   got_hashtag = JSON.load(get_hashtags.body)['hashtags'].join(',')
 %Q{
+  <!-- Function for remove the spaces in the input field -->
+  <script language="javascript" type="text/javascript">
+  function removeSpacesfromstring(string) 
+  {
+  return string.split(' ').join('');
+  }
+  </script>
+  <!-- Input field for hashtags -->
 	<form method="post" action="/json">
-	<p> Enter here your hashtags: <input type ="text" name ="hashtagform" value=#{got_hashtag}></p>
+	<p> Enter here your hashtags: <input type ="text" onblur ="this.value=removeSpacesfromstring(this.value);" name ="hashtagform" value=#{got_hashtag}></p>
 	<input type="submit">
 	</form>
+  <!-- delete button for the hashtags -->
   <form method="post" action="/jsond">
   <button type="submit" value="Submit">Delete</button>
   </form>
